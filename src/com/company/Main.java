b1ddbda6ce7f5b3b9de7fa6aaecdceea76fe5aaf
macnailyat <macnailyat@gmail.com>
2021-11-03 20:14:23 -0400

    the white tesla from stashing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
+        car.name = "Tesla";
+        car.color = "White";
+        car.doors = 4;
+        car.engineCapacity = "60000cc";
+
+        System.out.println(car.color);
 
 
     }
